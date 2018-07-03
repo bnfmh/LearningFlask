45adfac937b2b7346090c98c11f07101d0404950
HBT <HBT@HBT-LAPTOP>
2018-07-03 08:54:31 +0430

    use template to render page (outline html)

--- BasicProject/HelloFlask/views.py
+++ BasicProject/HelloFlask/views.py
@@ -1,7 +1,23 @@
-from flask import Flask
+from datetime import datetime
+from flask import render_template
 from HelloFlask import app
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return "Hello Flask!"
+    now = datetime.now()
+    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+    #1
+    #html_content = "<html><head><title>Hello Flask</title></head><body>"
+    #html_content += "<strong>Hello Flask!</strong> on " + formatted_now
+    #html_content += "</body></html>"
+    #return html_content
+
+    #2
+    #return render_template("index.html",content = "<strong>Hello, Flask!</strong> on " + formatted_now)
+
+    #3
+    return render_template("index.html",
+                           content = " on " + formatted_now,
+                           message = "hello , flask",
+                           title = "Hello Flask")
